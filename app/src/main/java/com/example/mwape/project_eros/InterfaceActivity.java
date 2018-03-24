0a7227342b698fe41a1a71efdad3d92ed5a52ec4
Mwape Chintu <m.t.chintu@hotmail.com>
2018-03-25 00:14:51 +0100

    App spashscreen added

---
+++ app/src/main/java/com/example/mwape/project_eros/InterfaceActivity.java
@@ -0,0 +1,13 @@
+package com.example.mwape.project_eros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InterfaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interface_activity);
+    }
+}
